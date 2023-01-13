e98be3364e71c0d3d345237e461420bc531622b0
Phonixed456 <pawel.zdunowski123@gmail.com>
2023-01-13 12:50:35 +0000

    Created the project

---
+++ Losslessidiser/target/debug/deps/Losslessidiser-b422c541ba8892ef.d
@@ -0,0 +1,5 @@
+z:\.PYTHON_PROJECTS\Losslessidiser\Losslessidiser\target\debug\deps\Losslessidiser-b422c541ba8892ef.rmeta: src\main.rs
+
+z:\.PYTHON_PROJECTS\Losslessidiser\Losslessidiser\target\debug\deps\Losslessidiser-b422c541ba8892ef.d: src\main.rs
+
+src\main.rs:
